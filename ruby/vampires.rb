72cbeb57998fb62ea72162b16d4ba31a795cd40f
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-13 13:05:23 -0700

    release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts  "What year were you born?"
+year = gets.chomp
+
+puts  "Our company serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts  "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
