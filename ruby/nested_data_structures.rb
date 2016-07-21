362f89b190595728e3e407c88ef3aa7543cceade
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-21 10:47:11 -0700

    fixing errors

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,48 +4,53 @@ highway = {
 		driver_info: {
 			age: 43,
 			speeding_tickets: 4
-		}
-		passengers: [
-			"Jamal Sleeve", "Liza Gordon"
+		},
+		passengers:[
+			"Jamal Sleeve", 
+			"Liza Gordon"
 		]
-	}
+	},
 	car_2: {
 		driver: 'Tim Buck II',
 		driver_info: {
 			age: 17,
 			speeding_tickets: 0
-		}
-		passengers: [
+		},
+		passengers:[
 			"Leeroy Jenkins"
 		]
-	}
+	},
 	car_3: {
 		driver: 'Adam Frill',
-		driver_info: {
+		driver_info:{
 			age: 29,
 			speeding_tickets: 12
-		}
-		passengers: [
-			"Maggie Frill", "Jimmy Frill"
+		},
+		passengers:[
+			"Maggie Frill", 
+			"Jimmy Frill"
 		]
-	}
+	},
 	car_4: {
-		driver: 'Bilbo Baggins',
+		driver: 'Frodo Baggins',
 		driver_info: {
 			age: 80,
 			speeding_tickets: 31
-		}
-		passengers: [
-			"Sam", "Pippin", "Mary"
+		},
+		passengers:[
+			"Sam", 
+			"Pippin", 
+			"Mary"
 		]
-	}
+	},
 	car_5: {
 		driver: 'Harry Lawrence',
 		driver_info: {
 			age: 20,
 			speeding_tickets: 1
-		}
-		passengers: [
+		},
+		passengers:[
 		]
 	}
 }
+highway
