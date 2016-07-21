71df3d45273bf8028541ddec77069efd355eff40
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-21 10:29:28 -0700

    release 1

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,51 @@
+highway = {
+	car_1: {
+		driver: 'Steve Day',
+		driver_info: {
+			age: 43,
+			speeding_tickets: 4
+		}
+		passengers: [
+			"Jamal Sleeve", "Liza Gordon"
+		]
+	}
+	car_2: {
+		driver: 'Tim Buck II',
+		driver_info: {
+			age: 17,
+			speeding_tickets: 0
+		}
+		passengers: [
+			"Leeroy Jenkins"
+		]
+	}
+	car_3: {
+		driver: 'Adam Frill',
+		driver_info: {
+			age: 29,
+			speeding_tickets: 12
+		}
+		passengers: [
+			"Maggie Frill", "Jimmy Frill"
+		]
+	}
+	car_4: {
+		driver: 'Bilbo Baggins',
+		driver_info: {
+			age: 80,
+			speeding_tickets: 31
+		}
+		passengers: [
+			"Sam", "Pippin", "Mary"
+		]
+	}
+	car_5: {
+		driver: 'Harry Lawrence',
+		driver_info: {
+			age: 20,
+			speeding_tickets: 1
+		}
+		passengers: [
+		]
+	}
+}
