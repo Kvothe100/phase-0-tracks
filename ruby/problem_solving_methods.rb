be41523cb0bc9288726cc8c2c1092d64ebbfd7de
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-24 22:53:20 -0700

    release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,4 @@
+
 def fib(n)
 	array = [0,1]
 	while array.length < n
@@ -8,3 +9,24 @@ end
 
 
 puts fib(10)
+
+#define our sorting code method
+def bubble_sort(array)
+	n = array.length
+#make our first loop
+	loop do 
+		swapped = false
+#if element on the right is greater than the element on the right--swap them.
+		(n-1).times do |i|
+			if array[i] > array[i + 1]
+				array[i], array[i + 1] = array[i + 1], array[i]#this is the swap
+				swapped = true
+			end
+		end
+		break if not swapped
+	end
+	array
+end
+#test our sort with an array!
+a=[1,4,1,3,4,3]
+bubble_sort(a)
