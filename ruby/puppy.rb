574ee6bd2b9c4ee3093f904b80fb30d2d0a036fb
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-29 09:20:20 -0700

    class comment

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#SPECIES-------
+#Boxer
+
+#CHARACTERISTICS-------
+#Fur length: Short
+#Fur color: Fawn or Brindle
+#Tail size: Short
+#Size: medium/large
+
+#BEHAVIOR--------
+#Run
+#jump
+#Sleep
+#Eat
