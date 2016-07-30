207106ed48981dfcd073fe350c1850885d9646d9
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-30 10:59:03 -0700

    release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,25 @@ class Puppy
     toy
   end
 
-end
+  def speak(n)
+    puts "woof!"*n
+  end
 
+  def rolls_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(num)
+  	p num*7
+  end
+  def new_name(name)
+    puts "Spot preferes to be called #{name}."
+  end
+
+end
+spot=Puppy.new
+spot.fetch("ball")
+spot.speak(6)
+spot.rolls_over
+spot.dog_years(6)
+spot.new_name("James")
