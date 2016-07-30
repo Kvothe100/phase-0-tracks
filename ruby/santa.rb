e4bf255196a49a5cb9893e685f1aee10641f5d66
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-30 16:18:57 -0700

    release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,15 +1,30 @@
 class Santa 
+	@reindeer_ranking= ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	@age= 0
 	def speak
 		puts "Ho, Ho, ho! Haaaappy holidays!"
 	end
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
-	def initialize
+	def initialize(gender, ethnicity,)
+		@gender= gender
+		@ethnicity= ethnicity
 		p "Initializing Santa instane..."
+	end
+	def celebrate_birthday(new_age)
+		@age= new_age
 	end
 end
 
-claus=Santa.new
+claus=Santa.new("male", "other")
 claus.speak
 claus.eat_milk_and_cookies(choc chip)
+santas=[]
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
