d92858559ae952aef0835293f0dfc252f1a40ba9
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-30 19:27:30 -0700

    release 1

---
+++ ruby/shout.rb
@@ -0,0 +1,10 @@
+module Shout
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+	def self.yelling_happily(words)
+		words + "!!!" + " :)"
+	end
+end
+self.yell_angrily("YOU ARE A SMELLY PERSON")
+self.yelling_happily("WOO! THOSE ARE MY LOTTO NUMBERS")
