8e0958a0acbc63721e0fc2b6454608137258336c
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-31 17:33:53 -0700

    fixed many lil errors

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -16,7 +16,7 @@ class Game
 	def secret(word)
 		@secret_word= word
 		word.length.times {word_guessed << '_'}
-		@how_many_guesses = words.length * 3
+		@how_many_guesses = word.length * 3
 	end
 	#time to start letting them guess letters!
 	#I'll need to use the handy .include?
@@ -38,40 +38,41 @@ hangperson.secret(gets.chomp)
 #start your guessing!!
 until hangperson.guessed_letters.length == hangperson.how_many_guesses || !hangperson.word_guessed.include?('_')
 	puts "Guess a letter"
-	guess= gets.chomp
-	if guess.is_a?(string) && guess.length ==1
-		if hangperson.guessed_letters.include? guess
+	pick= gets.chomp
+	if pick.is_a?(String) && pick.length == 1
+		if hangperson.guessed_letters.include? pick
 			puts "Hey bub, you already guessed that letter!"
-		elsif hangperson.guess(guess)
+		elsif hangperson.pick(pick)
 			already_guessed = []
 			i=0
-			hangperson.secret_word.each_char
-				do |letter|
-				if letter == guess
-				already_guessed << i
+			hangperson.secret_word.each_char do |letter|
+				if letter == pick
+					already_guessed << i
 				end
 				i+=1
 			end 
 			already_guessed.each do |location|
-				hangperson.word_guessed[location] = guess
+				hangperson.word_guessed[location] = pick
 			end
-			hangperson.guessed_letters << guess
+			hangperson.guessed_letters << pick
 			# now we inform them they are sooooo awesome
-			puts "You are soooo awesome at guessing letters. Well done, chap!"
-			p hangperson.word_guessed.join(' ')
+			puts "You got a letter!"
+			puts hangperson.word_guessed.join(' ')
 			#inform them them they didn't guess a letter in the word....
 		else
-			hangperson.guessed_letters << guess
+			hangperson.guessed_letters << pick
 			puts "So sorry, that letter is not in our secret word..."
-			puts hangperson.word.guessed.include? ('_')
+			puts hangperson.word_guessed.join (' ')
 		end
 		#now to catch any errors in our input from the user
 	else
 		puts "That was an invalid input. Please just put in one letter. Thanks!"
 	end
+end
 	#deliver the bad news about our person
-	if hangperson.word_guessed.include? '_'
+if hangperson.word_guessed.include? '_'
 		puts "I'm so sorry. Our person has been.....hung."
 	#now the good news! 
-	else
+else
 		puts "YOU DID IT! Our person has been saved.........for now :)"
+	end
