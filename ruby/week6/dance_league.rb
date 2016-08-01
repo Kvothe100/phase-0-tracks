2bef274a56210e72f2847d87348182d8522103f1
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-31 23:14:29 -0700

    release 2

---
+++ ruby/week6/dance_league.rb
@@ -0,0 +1,24 @@
+class Dancer
+	attr_accessor :arr, :age, :name
+	def initialize(arr)
+		@arr = arr
+		@name = name
+		@age = age
+	end
+	def name(name,age)
+		@name
+	end
+	def age(name,age)
+		@age
+	end
+	def age(name, age)
+		@age
+	end
+	def pirouette
+		puts "*twirls"
+	end
+	def bow
+		puts "bows to partner."
+	end
+
+end
