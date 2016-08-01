b0700bb93a1f1f7db14be64516700470a1560275
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-07-31 23:20:02 -0700

    ..sort of finished

--- ruby/week6/dance_league.rb
+++ ruby/week6/dance_league.rb
@@ -20,5 +20,17 @@ class Dancer
 	def bow
 		puts "bows to partner."
 	end
+	def leaps
+		puts "Leaps in the air!"
+	end
 
 end
+#I ran into a major problem with my rspec. It will not run my tests. 
+#it keeps saying 'ErrorLoad' and 'block in load_spec' when I tried 
+#to run my tests. Very verrrryy frustrated right now because I wasted 
+#over an hour trying to troubleshoot the issue with no luck
+#I know my score will suffer but I care more about fixing the issue
+#So any insight you have into it would be greatly appreciated.
+#I feel like I know this material well enough and I know my score
+#on this will be lower becuase I didn't complete all of the questions
+#but I really care about fixing my issue with rspec. 
