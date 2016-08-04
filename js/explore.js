422a33162c08c1205c63134370f132bad9f38cd1
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-04 11:07:10 -0700

    pseudocode

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// create a function that takes a string as an argument
+// for each letter in the string split them
+//reverse the letters and print the result
+//store the result in a variable
+//print the variable inside of a string
