805589ecefb2266f7aff0c14ceb53488b2ba76cd
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-05 10:30:15 -0700

    pseudocoded

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+//create a function
+//input: an array of strings
+//output: the longest string in the array
+
+//check the length of the each word/phrase and compare it to
+//the others
+
+//return the longest string
+var array = ["Start me up", "Beast of Burden", "Can't you hear me knocking?", "Let it bleed"];
+function longestString();
