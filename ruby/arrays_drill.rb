6ba366389a0f673ed9594e8206ce0c2baa006530
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-11 12:17:12 -0700

    added 2 methods

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,16 @@
+def add_to_array(array, item)
+	array.push(item)
+end
+
+p add_to_array(["fix", 3, "its"], "array")
+
+def build_array(x, y, z)
+	new_array= []
+	new_array.push(x, y, z)
+end
+
+p build_array(true, "yes", 4)
+
 bedroom = []
 p bedroom
 
