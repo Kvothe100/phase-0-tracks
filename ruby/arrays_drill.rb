c65597cf12f9c4ee8cddb9f8acaab2e8fa1a441a
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-11 11:48:03 -0700

    combined arrays

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -9,3 +9,18 @@ p bedroom
 
 bedroom.insert(2, "Shoes")
 p bedroom
+
+bedroom.shift
+p bedroom
+
+bedroom.include?("Fan")
+	if true
+		puts "The item is in the array!"
+	else
+		puts "The item is NOT in the array!"
+	end
+
+clothes = ["Shirt", "Hat", "Pants"]
+
+both = bedroom + clothes
+p both
