0ec427a2d78f97e0536b6c45892ad4a139f79305
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-12 10:16:03 -0700

    Added user prompt

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,45 +1,24 @@
-#prompt user with questions about the design they desire
+#First create a prompt to gather initial data
+#with some questions and gets.chomp
 
-puts "What is your name?"
-name = gets.chomp 
+puts "Your name, please?"
+name = gets.chomp
+
 puts "What is your age?"
-age = gets.to_i
-puts "How many children do you have? Use a number please."
-num_kids= gets.to_i
-puts "What type of decor theme do you want?"
-decor_theme = gets.chomp
+age = gets.chomp
 
-#use keys to store the data in a hash
+puts "How many children do you have?"
+children = gets.chomp
+
+puts "What sort of decor them would you like?"
+theme = gets.chomp
+
+puts "Your price range is over 500,000? (true/false)"
+price = gets.chomp
+
+#set the data in a hash with the info from our prompt
+#using keys
+
 decor = {
-	:user_name => name,
-	:user_age => age,
-	:kids => num_kids,
-	:theme => decor_theme
+		:
 }
-p decor #prints the data for the user and designer to read
-
-puts "What answer you like to update?" #Ask if the user would like to change anything
-update= gets.chomp
-if update== "name"
-	puts "What is your new name?"
-		new_name=gets.chomp
-	decor[:user_name] = new_name
-elsif update== "age"
-	puts "What is the new age?"
-		new_age= gets.to_i
-	decor[:user_age] = new_age
-elsif update == "num_kids"
-	puts "What is the new number of kids?"
-		new_kids= gets.to_i
-	decor[:kids] = new_kids
-elsif update =="decor_theme"
-	puts "What is your new theme?"
-		new_theme= gets.chomp
-	decor[:theme] = new_theme
-else
-	puts "Thanks! Have a great day"
-end
-p decor #prints updated hash
-
-
-
