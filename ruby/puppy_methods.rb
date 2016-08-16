d3abe4ed12cf815b2e0654048a4c14a8396235c6
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-16 11:18:57 -0700

    Added puppy class/driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,56 +4,7 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
-  def speak(n)
-    puts "woof!"*n
-  end
-
-  def rolls_over
-  	puts "*rolls over*"
-  end
-
-  def dog_years(num)
-  	p num*7
-  end
-  def new_name(name)
-    puts "Spot preferes to be called #{name}."
-  end
-  def initialize
-    p "initializing new puppy instance..."
-  end
-
-end
-spot=Puppy.new
-spot.fetch("ball")
-spot.speak(6)
-spot.rolls_over
-spot.dog_years(6)
-spot.new_name("James")
-
-class Hockey
-  def initialize
-    puts "Initializing new player..."
-  end
-  def goals(g)
-    puts "Our player has scored #{g} goals."
-  end
-  def teeth
-    puts "*Gets teeth knocked out*"
-  end
 end
 
-goon=Hockey.new
-goon.goals(78)
-goon.teeth
-
-players=[]
-
-50.times do
-  players<< Hockey.new
-end
-
-players.each do |puck|
-  puck.teeth
-  puck.goals(rand 1..50)
-end
+fido = Puppy.new
+fido.fetch("ball")
