20f8b7a1e2ada1fcbfcecec86cf2bed6b74401af
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-16 10:47:27 -0700

    commented puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+#SPECIES-------
+#Puppy
+
+#CHARACTERISTICS-------
+#Fur length: Short
+#Fur color: Fawn or Brindle
+#Tail size: Short
+#Size: medium/large
+
+#BEHAVIOR--------
+#Run
+#jump
+#Sleep
+#Eat
+
+
+
+
+#This appears to be the only thing we had to do for 6.1? Unless I missed something.
