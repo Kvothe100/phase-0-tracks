327026433276d9ff5a4c704ebc22d5327a8216f9
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-16 11:28:48 -0700

    added own class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+  def initialize
+    p "Initializing new puppy instance..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -29,3 +33,19 @@ fido.speak(4)
 fido.roll_over
 fido.dog_years(5)
 fido.new_name("Tim")
+
+class Hockey
+  def initialize
+    puts "Initializing new player..."
+  end
+  def goals(g)
+    puts "Our player has scored #{g} goals."
+  end
+  def teeth
+    puts "*Gets teeth knocked out*"
+  end
+end
+
+goon=Hockey.new
+goon.goals(78)
+goon.teeth
