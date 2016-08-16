ae1ea5765d5c29b6e3e962c25ba3d8abbb72c18b
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-16 11:24:51 -0700

    added more methods/driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,7 +4,28 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+
+  def speak(n)
+    puts "Woof!"*n 
+  end
+
+  def roll_over
+    p "**rolls over**"
+  end
+
+  def dog_years(num)
+    p num*7
+  end
+
+  def new_name(name)
+    puts "Our dog prefers to be called #{name}."
+  end
+
 end
 
 fido = Puppy.new
 fido.fetch("ball")
+fido.speak(4)
+fido.roll_over
+fido.dog_years(5)
+fido.new_name("Tim")
