908cb1fd4a10445ca5e39383dada615954cb62ac
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-17 10:17:07 -0700

    Made santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,47 +1,14 @@
-class Santa 
-	attr_accessor :age, :ethnicity
+class Santa
 
-	@reindeer_ranking= ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	@age= 0
+	def initialize
+		p "Initializing Santa instance...."
+	end
+
 	def speak
-		puts "Ho, Ho, ho! Haaaappy holidays!"
+		p "Ho, ho, ho! Haaaaapppy holidays!"
 	end
+
 	def eat_milk_and_cookies(cookie)
-		puts "That was a good #{cookie} cookie!"
+		puts "That was a good #{cookie} cookie"
 	end
-	def initialize(gender, ethnicity)
-		@gender= gender
-		@ethnicity= ethnicity
-		p "Initializing Santa instance..."
-	end
-	def celebrate_birthday(new_age)
-		@age= new_age
-	end
-	def gender=(new_gender)
-		@gender = new_gender
-	end
-	
-	puts "Reindeer ranking is currently #{@reindeer_ranking}"
-end		
-
-santas=[]
-
-santas << claus=Santa.new("male", "other")
-claus.speak
-claus.eat_milk_and_cookies("choc chip")
-claus.gender= "N/A"
-
-gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-gender.length.times do |i|
-	santas << Santa.new(gender[i], ethnicity[i])
 end
-
-100.times do
-	santas << Santa.new(gender.shuffle.sample, ethnicity.shuffle.sample)
-end
-santas.each do |sanda|
-	sanda.celebrate_birthday(rand 0..140)
-end
-p santas
-
