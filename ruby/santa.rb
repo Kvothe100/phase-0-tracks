110649f6aa7c8ebef3fb86fdedd617083f46982b
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-17 11:05:36 -0700

    Added attr's

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,6 @@
 class Santa
+	  attr_accessor :gender,:age
+  attr_reader  :reindeer_ranking,:ethnicity
 
 	def initialize(gender, ethnicity)
 		p "Initializing Santa instance...."
@@ -15,6 +17,19 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie} cookie"
 	end
+
+	def celebrate_birthday
+		@age += 1
+		puts "Santa is now #{@age} year(s) old."
+	end
+
+	def get_made_at(reindeer)
+		if @reindeer_ranking.include?(reindeer)
+			@reindeer_ranking.delete(reindeer)
+			@reindeer_ranking << reindeer
+		else
+			puts "That reindeer is not with us!"
+		end
 end
 
 new_santa = Santa.new
