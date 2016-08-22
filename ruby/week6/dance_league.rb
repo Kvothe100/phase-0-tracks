2d315590c4443be281f9382a492bdeb6210de0a2
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-22 08:44:22 -0700

    added dance card

--- ruby/week6/dance_league.rb
+++ ruby/week6/dance_league.rb
@@ -1,8 +1,9 @@
 class Dancer
-	attr_accessor :age, :name
+	attr_accessor :age, :name, :card
 	def initialize(name, age)
 		@name = name
 		@age = age
+		@card = []
 	end
 
 	def pirouette
@@ -14,7 +15,12 @@ class Dancer
 	end
 
 	def leaps
-		puts "Leaps in the air!"
+		p "Leaps in the air!"
+	end
+
+	def queue_dance_with(dancer)
+		@card << dancer
+		@card
 	end
 
 end
