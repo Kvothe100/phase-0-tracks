70f684fd441c2a153efce7b50678e2b2b43be1e0
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-28 19:36:10 -0700

    added js code

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,16 @@
 //reverse the letters and print the result
 //store the result in a variable
 //print the variable inside of a string
+function reverse(s) {
+	return s.split('').reverse().join('');
+}
+
+myVar =  reverse("kingofthenorf");
+
+console.log(reverse("kingofthenorf"));
+console.log("The reversed word is " + myVar);
+
+var newVar = 1;
+if (newVar >= 1) {
+	console.log(myVar);
+}
