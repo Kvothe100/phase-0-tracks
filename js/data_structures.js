7a55409128c24f96eba788a7f61f5cbf11673ae1
Tom O'FLaherty <teoflaherty86@gmail.com>
2016-08-28 19:46:31 -0700

    added car function

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,23 @@ for (var i = 0; i < horseNames.length; i++) {
 	horses[horseNames[i]] = colors[i];
 }
 console.log(horses);
+
+// create car function
+function car (color, year, make){
+	console.log("our car is a:", this);
+
+	this.color = color;
+	this.year = year;
+	this.make = make;
+	this.wheels = function() {console.log("Wow! It's got wheels!"); };
+
+	console.log("Car creator is finished");
+}
+console.log("Time to build a car!");
+
+var newCar  = new car("Red", 2009, "Toyota");
+console.log(newCar);
+newCar.wheels();
+var newCar2 = new car("Blue", 1972, "Chevy");
+console.log(newCar2);
+newCar2.wheels();
